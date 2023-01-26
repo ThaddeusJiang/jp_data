6a7feee55f70242a5211544c16a286036ab4b84c
TJ <thaddeusjiang@gmail.com>
2023-01-26 21:21:17 +0900

    refactor

--- lib/jp_data/zipcode.ex
+++ lib/jp_data/zipcode.ex
@@ -5,4 +5,46 @@ defmodule JpData.Zipcode do
     JpData.Address.Zipcode
     |> JpData.Repo.get_by(zipcode: zipcode)
   end
+
+  # read zipcode data from CSV file
+  def read_zipcode_data do
+    File.stream!(
+      Path.join(:code.priv_dir(:jp_data), "repo/zipcode.csv"),
+      [read_ahead: 124_429],
+      124_429
+    )
+    |> CSV.decode()
+    |> Enum.map(fn row -> parse_zipcode(row) end)
+  end
+
+  defp parse_zipcode({:ok, row}) do
+    # [_, _, zipcode, pref_kana, city_kana, town_kana, pref, city, town, _, _, _, _, _, _, _] = row
+    [
+      _,
+      _,
+      zipcode,
+      pref_kana,
+      city_kana,
+      town_kana,
+      pref,
+      city,
+      town,
+      _,
+      _,
+      _,
+      _,
+      _,
+      _
+    ] = row
+
+    %{
+      zipcode: zipcode,
+      pref_kana: pref_kana,
+      city_kana: city_kana,
+      town_kana: town_kana,
+      pref: pref,
+      city: city,
+      town: town
+    }
+  end
 end
--- priv/repo/zipcodes.exs
+++ priv/repo/zipcodes.exs
@@ -1,46 +1,4 @@
-# read zipcode data from CSV file
-def read_zipcode_data do
-  File.stream!(
-    Path.join(:code.priv_dir(:jp_data), "repo/zipcode.csv"),
-    [read_ahead: 124_429],
-    124_429
-  )
-  |> CSV.decode()
-  |> Enum.map(fn row -> parse_zipcode(row) end)
-end
-
-defp parse_zipcode({:ok, row}) do
-  # [_, _, zipcode, pref_kana, city_kana, town_kana, pref, city, town, _, _, _, _, _, _, _] = row
-  [
-    _,
-    _,
-    zipcode,
-    pref_kana,
-    city_kana,
-    town_kana,
-    pref,
-    city,
-    town,
-    _,
-    _,
-    _,
-    _,
-    _,
-    _
-  ] = row
-
-  %{
-    zipcode: zipcode,
-    pref_kana: pref_kana,
-    city_kana: city_kana,
-    town_kana: town_kana,
-    pref: pref,
-    city: city,
-    town: town
-  }
-end
-
-zipcodes = read_zipcode_data()
+zipcodes = JpData.Zipcode.read_zipcode_data()
 alias JpData.Repo
 
 Enum.each(zipcodes, fn data ->
